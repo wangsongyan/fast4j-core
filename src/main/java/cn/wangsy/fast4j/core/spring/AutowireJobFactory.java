5755c507e5b5990a53d17343addcb2c2a2446316
wangsongyan <1104237534@qq.com>
2016-09-06 13:01:57 +0800

    Quartz JobFactory不支持Spring注入的问题

---
+++ src/main/java/cn/wangsy/fast4j/core/spring/AutowireJobFactory.java
@@ -0,0 +1,29 @@
+package cn.wangsy.fast4j.core.spring;
+
+import org.quartz.spi.TriggerFiredBundle;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
+import org.springframework.scheduling.quartz.AdaptableJobFactory;
+
+//<bean id="jobFactory" class="cn.wangsy.fast4j.core.spring.AutowireJobFactory"></bean>
+//<bean name="quartzScheduler" class="org.springframework.scheduling.quartz.SchedulerFactoryBean">
+//	<property name="jobFactory" ref="jobFactory"/>
+//	<property name=../>
+//</bean>
+/**
+ * 说明：
+ * @author wangsy
+ * @date 创建时间：2016年9月6日 下午12:54:28
+ */
+public class AutowireJobFactory extends AdaptableJobFactory {
+
+	@Autowired
+	private AutowireCapableBeanFactory capableBeanFactory;
+
+	protected Object createJobInstance(TriggerFiredBundle bundle)
+			throws Exception {
+		Object jobInstance = super.createJobInstance(bundle);
+		capableBeanFactory.autowireBean(jobInstance);
+		return jobInstance;
+	}
+}
