2778508abeb03e39d7a0de0719e6f2e07a32fc8c
wangsongyan <1104237534@qq.com>
2016-11-11 11:02:02 +0800

    提交excel注解类

---
+++ src/main/java/cn/wangsy/fast4j/core/annotation/ExcelField.java
@@ -0,0 +1,38 @@
+/**
+ * 
+ */
+package cn.wangsy.fast4j.core.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * excel字段注解类
+ * @author wangsy
+ * @date 2016年11月11日上午10:48:28
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+public @interface ExcelField {
+
+	/***
+	 * 列名
+	 * @return
+	 */
+	public String columnName();
+	
+	/***
+	 * 排序字段
+	 * @return
+	 */
+	public int sort() default -1;
+	
+	/***
+	 * 列宽
+	 * @return
+	 */
+	public float width() default 200;
+
+}
