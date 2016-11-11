516092655c689ea2df090cf52b6db399428f1f3e
wangsongyan <1104237534@qq.com>
2016-11-11 12:39:09 +0800

    添加poi maven依赖

--- src/main/java/cn/wangsy/fast4j/util/ExcelUtil.java
+++ src/main/java/cn/wangsy/fast4j/util/ExcelUtil.java
@@ -4,6 +4,8 @@
 package cn.wangsy.fast4j.util;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.HashMap;
 
 import cn.wangsy.fast4j.core.annotation.ExcelField;
 
@@ -15,7 +17,9 @@ import cn.wangsy.fast4j.core.annotation.ExcelField;
 public class ExcelUtil<T> {
 
 	
-	public static void getAnnotation(Class<?> clazz){
+	public static HashMap<String, Object> getAnnotation(Class<?> clazz){
+		
+		HashMap<String, Object> map = new HashMap<String, Object>();
 		
 		for(;clazz != Object.class;clazz = clazz.getSuperclass()){
 			
@@ -24,11 +28,28 @@ public class ExcelUtil<T> {
 				ExcelField annotation = field.getAnnotation(ExcelField.class);
 				if(null != annotation){
 					System.out.println("columnName:"+annotation.columnName()+",sort:"+annotation.sort()+",width:"+annotation.width());
+					
+					String fieldname = field.getName();
+					String setMethodName = "set"
+							+ fieldname.substring(0, 1).toUpperCase()
+							+ fieldname.substring(1);
+					// 构造调用的method
+					Method setMethod = null;
+					try {
+						setMethod = clazz.getMethod(setMethodName,
+								new Class[] { field.getType() });
+						map.put(annotation.columnName(), setMethod);
+					} catch (NoSuchMethodException e) {
+						e.printStackTrace();
+					} catch (SecurityException e) {
+						e.printStackTrace();
+					}
 				}
 			}
 			
 		}
 		
+		return map;
 	}
 	
 }
