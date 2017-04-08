c6a1ce95496367a71773e84ee25ee976c59c2e1d
wangsongyan <1104237534@qq.com>
2017-04-08 16:12:49 +0800

    添加setter/getter方法

--- src/main/java/cn/wangsy/fast4j/core/exception/ParamValidException.java
+++ src/main/java/cn/wangsy/fast4j/core/exception/ParamValidException.java
@@ -7,21 +7,29 @@ import java.util.List;
 
 import cn.wangsy.fast4j.core.entity.FieldError;
 
-
 /**
  * @author wangsy
  * @date 2017年3月14日下午4:35:22
  */
-public class ParamValidException extends Exception{
+public class ParamValidException extends Exception {
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	
+
 	private List<FieldError> fieldErrors;
-    public ParamValidException(List<FieldError> errors) {
-        this.fieldErrors = errors;
-    }
-	
+
+	public ParamValidException(List<FieldError> errors) {
+		this.fieldErrors = errors;
+	}
+
+	public List<FieldError> getFieldErrors() {
+		return fieldErrors;
+	}
+
+	public void setFieldErrors(List<FieldError> fieldErrors) {
+		this.fieldErrors = fieldErrors;
+	}
+
 }
